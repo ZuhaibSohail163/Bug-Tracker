64a3aca3ff13359c9110c2957dd9da0320d81f50
ZuhaibSohail <105937119+ZuhaibSohail@users.noreply.github.com>
2022-12-14 22:49:27 +0500

    added performance testing

--- backend/__tests__/api/api_spec.js
+++ backend/__tests__/api/api_spec.js
@@ -1,15 +1,49 @@
 const frisby = require('frisby');
 
-const frisby = require('frisby');
-
+//register
 it ('POST should return a status of 201 Created', function () {
   return frisby
-    .post('localhost:3000/auth/register', {
-      first_name: 'saifi',
-      last_name: 'billa',
-      phone: '101210201',
-      email: 'saifi@gmail.com',
-      password: '12345',
+    .post('http://localhost:3000/register?redirect=/', {
+      Name:'Ali Abdullah',
+      email:'ali@gmail.com',
+      password: "11111111",
+      confirmPassword: '11111111'
     })
     .expect('status', 201);
+});
+
+//login
+it ('POST should return a status of 201 Created', function () {
+  return frisby
+    .post('http://localhost:3000/login', {
+      email:'ali@gmail.com',
+      password: "11111111",
+    })
+    .expect('status', 201);
+});
+
+//UPDATE PROFILE
+it ('POST should return a status of 200 OK', function () {
+  return frisby
+    .put('http://localhost:3000/profile', {
+      Name: 'My Updated Name',
+      email: 'updatted@gmail.com',
+      password:'123',
+      Confirmpassword:'123'
+    })
+    .expect('status', 200);
+});
+
+//SHOW PRODUCTS
+it ('GET should return a status of 200 OK', function () {
+  return frisby
+    .get('http://localhost:3000/Shop')
+    .expect('status', 200);
+});
+
+//SHOW PRODUCTS WITH ADMIN
+it ('GET should return a status of 200 OK', function () {
+  return frisby
+    .get('http://localhost:3000/admin/productlist')
+    .expect('status', 200);
 });
---
+++ frontend/cypress/integration/1-getting-started/cypress-audit-specs.js
@@ -0,0 +1,29 @@
+/// <reference types="Cypress" />
+
+describe('Photobiz Lighthouse', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/register?redirect=/');
+    });
+  
+    it('should run performance audits', () => {
+      cy.lighthouse();
+    });
+  
+    it('should run performance audits using custom thresholds', () => {
+      const customThresholds = {
+        performance: 50,
+        accessibility: 50,
+        seo: 70,
+        'first-contentful-paint': 2000,
+        'largest-contentful-paint': 3000,
+        'cumulative-layout-shift': 0.1,
+        'total-blocking-time': 500,
+      };
+  
+      const desktopConfig = {
+        formFactor: 'desktop',
+        screenEmulation: { disabled: true },
+      };
+      cy.lighthouse(customThresholds, desktopConfig);
+    });
+});
